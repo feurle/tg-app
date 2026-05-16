54cb504dc48d4ef4d65df93bc65d99776a7b53b1
Daniel Feurle <dfeurle@yandex.com>
2026-05-16 09:41:35 +0200

    extended mail sending

--- src/main/java/com/feurle/tg/contact/infrastructure/rest/ContactController.java
+++ src/main/java/com/feurle/tg/contact/infrastructure/rest/ContactController.java
@@ -6,6 +6,7 @@ import com.feurle.tg.contact.application.ContactService;
 import com.feurle.tg.contact.infrastructure.rest.dto.SendMessageRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,13 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/contact")
 @RequiredArgsConstructor
+@Slf4j
 public class ContactController {
 
   private final ContactService contactService;
 
   @PostMapping("/message")
   public ResponseEntity<Void> sendMessage(@Valid @RequestBody SendMessageRequest request) {
-    contactService.sendMessage(request.title(), request.text(), request.replyToEmail());
+    log.info("sendMessage: GET /api/contact/message");
+    contactService.sendMessage(request.title(), request.text(), request.replyToEmail(), request.senderName());
     return ResponseEntity.noContent().build();
   }
 }
